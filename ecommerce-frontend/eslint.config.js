10db8c2c38b8e697aad49d4a8f8eeb9f7fd65a54
Juan Barragán <orionmaster8@gmail.com>
2025-07-12 13:31:42 -0500

    cambios lint

--- ecommerce-frontend/eslint.config.js
+++ ecommerce-frontend/eslint.config.js
@@ -1,15 +1,42 @@
+import eslintRecommended from 'eslint/conf/eslint-recommended.js';
 import vue from 'eslint-plugin-vue';
 import { FlatCompat } from '@eslint/eslintrc';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
-const compat = new FlatCompat();
+const compat = new FlatCompat({
+  recommendedConfig: eslintRecommended
+});
 
 export default [
   ...compat.extends('eslint:recommended'),
   {
+    files: ['*.ts', '**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+  {
     files: ['*.vue', '**/*.vue'],
     plugins: { vue },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
     rules: {
-      'vue/html-self-closing': 'error',
+      'vue/multi-word-component-names': 'off',
     },
   },
 ];
